f2476defea01f8c7b539ace62aa215ef72cbcfae
FernandoReyes06 <30992039+FernandoReyes06@users.noreply.github.com>
2019-04-08 01:55:08 -0500

    Find y FindAll actualizados

--- Simix/src/main/java/com/xiutech/simix/modelo/AbstractDAO.java
+++ Simix/src/main/java/com/xiutech/simix/modelo/AbstractDAO.java
@@ -73,7 +73,7 @@ public abstract class AbstractDAO<T> {
     
     protected T find(Class clazz, Serializable id){
         T obj = null;
-        Session session = this.sessionFactory.getCurrentSession();
+        Session session = this.sessionFactory.openSession();
         Transaction tx = null;
         try{
             tx = session.beginTransaction();
@@ -90,7 +90,7 @@ public abstract class AbstractDAO<T> {
     
     protected List<T> findAll(Class clazz){
         List<T> obj = null;
-        Session session = this.sessionFactory.getCurrentSession();
+        Session session = this.sessionFactory.openSession();
         Transaction tx = null;
         try{
             tx = session.beginTransaction();
